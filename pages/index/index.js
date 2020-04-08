81d1550b756a552ab4b2c5b8b9ae11bfa59679b9
chensheng <cson_chensheng@163.com>
2020-04-08 18:21:07 +0800

    fe: 增加内联标签的支持

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    htmlText: '<p><a href="https://www.baidu.com" target="_blank">测试跳转</a><br></p><p><img src="https://dev-sit-1251698455.cos.ap-guangzhou.myqcloud.com/ds/22/363/20200401/3c9e7798e3204756b9e0f3263882b81f.jpeg" style="max-width:100%;"><a href="https://baidu.com" target="_blank" style="background-color: rgb(255, 255, 255);">测试跳转</a><br></p>',
+    htmlText: '<p><a href="https://www.baidu.com" target="_blank">测试跳转</a>&nbsp;<span style="background-color: rgb(139, 170, 74);">sdgsd</span><br></p><p><img src="https://dev-sit-1251698455.cos.ap-guangzhou.myqcloud.com/ds/22/363/20200401/3c9e7798e3204756b9e0f3263882b81f.jpeg" style="max-width:100%;"><a href="https://baidu.com" target="_blank" style="background-color: rgb(255, 255, 255);">测试跳转</a><br></p>',
   },
 
   handleTagATap(url) {
